cb675ef4099602d0b5daf3494726dc93a8736fff
Sowmika-Arul <sowmikaarulkumar@gmail.com>
2024-08-24 19:24:15 +0530

    changes made

--- alumni1/src/App.js
+++ alumni1/src/App.js
@@ -1,8 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { ChatEngine } from 'react-chat-engine';
-import ChatFeed from './components/ChatFeed';
-import LoginForm from './components/LoginForm';
 import Login from './components/Login';
 import AlumniList from './components/AlumniList';
 import Home from './components/Home';
@@ -15,12 +12,6 @@ import ImageGallery from './components/Images';
 import './App.css';
 
 function App() {
-    if (!localStorage.getItem('username')) {
-        return (
-            <LoginForm />
-        );
-    }
-
     return (
         <Router>
             <Routes>
@@ -33,19 +24,6 @@ function App() {
                 <Route path="/alumni_details/:rollNo" element={<AlumniDetails />} />
                 <Route path="/events" element={<Events />} />
                 <Route path="/images" element={<ImageGallery />} />
-                {/* Chat Route */}
-                <Route
-                    path="/chat"
-                    element={
-                        <ChatEngine
-                            height="100vh"
-                            projectID="83613c3b-397a-4bd4-a528-c575ed4c1e0c"
-                            userName={localStorage.getItem('username')}
-                            userSecret={localStorage.getItem('password')}
-                            renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
-                        />
-                    }
-                />
             </Routes>
         </Router>
     );

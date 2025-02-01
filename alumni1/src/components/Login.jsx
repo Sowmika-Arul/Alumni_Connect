61562e60d07f71a134b1ba2a0af26afa8a0412c2
Sowmika-Arul <sowmikaarulkumar@gmail.com>
2025-02-01 19:27:18 +0530

    changes made

--- alumni1/src/components/Login.jsx
+++ alumni1/src/components/Login.jsx
@@ -12,7 +12,7 @@ function Login() {
 
 
         try {
-            const response = await fetch('http://localhost:5050/api/login', {
+            const response = await fetch('https://alumni-connect-5ad6.onrender.com/api/login', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
